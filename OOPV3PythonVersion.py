fba67955968ae0d47ee5b913a850689bcbaf83b2
John Stirling Stout <cuspcomputers@outlook.com>
2020-07-10 12:25:49 +0100

    Exercise on page 30 (note that brushes need considering, and that anything wth a Get or a Set can proably be done so these have been changed). Double buffering really needs sorting out

--- OOPV3PythonVersion.py
+++ OOPV3PythonVersion.py
@@ -10,41 +10,43 @@ class OOPDraw(wx.Frame):
     panel on which graphic elements are drawn """
 
     def __init__(self):
-        def AddChoice(panel: wx.Panel, vBox: wx.BoxSizer, label: str, y: int, options: List[str], handler: Callable):
+        def AddChoice(panel: wx.Panel, vBox: wx.BoxSizer, name: str, label: str, y: int, options: List[str], handler: Callable):
             vBox.Add(wx.StaticText(panel, wx.ID_ANY, label))
-            cb = wx.ComboBox(panel, wx.ID_ANY, options[0], choices=options)
-            cb.Bind(wx.EVT_COMBOBOX, handler, cb)
+            cb = wx.ComboBox(panel, wx.ID_ANY, options[0], choices=options, name=name)
+            cb.Bind(wx.EVT_TEXT, handler, cb)
             vBox.Add(cb)       
            
         wx.Frame.__init__(self, None, wx.ID_ANY, 'OOPDraw in Python', size=(800, 600))
-        self.SetSize(0, 0, 800, 600)
+        self.Size = (0, 0, 800, 600)
 
         # panel contains all the controls (label + combobox for each option)
         panel = wx.Panel(self, wx.ID_ANY)
         # vBox is responsible for sizing all the controls and placing them within panel
         vBox = wx.BoxSizer(wx.VERTICAL)
-        panel.SetSizer(vBox)
+        panel.Sizer = vBox
 
-        AddChoice(panel, vBox, "Line width:", 0, ["Thin", "Medium", "Thick"], self.OnLineWidthChanged)
-        AddChoice(panel, vBox, "Colour:", 30, ["Red", "Green", "Blue"], self.OnColourChanged)
-        #AddChoice(panel, vBox, "Shape:", 60, ["Line", "Rectangle", "Ellipse", "Circle"] , self.OnShapeChanged)
+        AddChoice(panel, vBox, "LineWidth", "Line width:", 0, ["Thin", "Medium", "Thick"], self.OnLineWidthChanged)
+        AddChoice(panel, vBox, "Colour", "Colour:", 30, ["Red", "Green", "Blue"], self.OnColourChanged)
+        AddChoice(panel, vBox, "Shape", "Shape:", 60, ["Line", "Rectangle", "Ellipse", "Circle"] , self.OnShapeChanged)
         #AddChoice(panel, vBox, "Action:", 90, ["Draw", "Select", "Duplicate", "Move", "Group"] , self.OnActionChanged)
 
         # hBox is responsible for sizing the panel for controls and the panel for drawing on (canvas)
         hBox = wx.BoxSizer(wx.HORIZONTAL)
-        self.SetSizer(hBox)
+        self.Sizer = hBox
         hBox.Add(panel, 0, wx.EXPAND)
         self.Canvas = wx.Panel(self, wx.ID_ANY, style=wx.BORDER_SIMPLE, name="Canvas")
         hBox.Add(self.Canvas, 1, wx.EXPAND)
 
-        # Page 6 changes start 
         self.Canvas.Bind(wx.EVT_PAINT, self.OnPaint)
        
-        wx.Window.SetDoubleBuffered(self, True)
+        self.DoubleBuffered = True 
         self.CurrentPen = wx.Pen(wx.BLACK)
-        # Page 6 changes end
+        self.CurrentBrush = wx.Brush(wx.BLACK, style=wx.BRUSHSTYLE_TRANSPARENT)
 
-        # Page 9 changes start
+        self.FindWindow("LineWidth").Value = "Medium"
+        self.FindWindow("Colour").Value = "Green"
+        self.FindWindow("Shape").Value = "Line"
+
         self.Canvas.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)
         self.Canvas.Bind(wx.EVT_LEFT_UP, self.OnMouseUp)
         self.Canvas.Bind(wx.EVT_MOTION, self.OnMouseMove)
@@ -52,8 +54,8 @@ class OOPDraw(wx.Frame):
         self.dragging = False
         self.startOfDrag = wx.Point()
         self.lastMousePosition = wx.Point()
-        self.Lines = []
-
+        self.Shapes = []
+       
     # These events could be deleted and the user asked to put them in
     def OnLineWidthChanged(self: wx.Frame, e: wx.Event):
         width = self.CurrentPen.Width
@@ -75,8 +77,8 @@ class OOPDraw(wx.Frame):
             colour = wx.BLUE
         self.CurrentPen = wx.Pen(colour, self.CurrentPen.Width)
 
-    #def OnShapeChanged(self: wx.Frame, e: wx.Event):
-    #    pass
+    def OnShapeChanged(self: wx.Frame, e: wx.Event):
+        pass
 
     #def OnActionChanged(self: wx.Frame, e: wx.Event):
     #    pass
@@ -84,15 +86,17 @@ class OOPDraw(wx.Frame):
     def OnPaint(self: wx.Frame, e: wx.Event):
         dc = wx.BufferedPaintDC(self.Canvas)
         dc.Clear()
-        for line in self.Lines:
-            line.Draw(dc)
-        rect = Rectangle(self.CurrentPen, 100, 200, 300, 500)
-        rect.Draw(dc)
+        dc.Brush = self.CurrentBrush
+        for shape in self.Shapes:
+            shape.Draw(dc)
 
     def OnMouseDown(self: wx.Window, e: wx.MouseEvent):
         self.dragging = True
-        self.startOfDrag = lastMousePosition = e.GetPosition()
-        self.Lines.append(Line(self.CurrentPen, e.GetPosition().x, e.GetPosition().y));
+        self.startOfDrag = lastMousePosition = e.Position
+        if self.FindWindow("Shape").Value == "Line":
+            self.Shapes.append(Line(self.CurrentPen, e.Position.x, e.Position.y));
+        elif self.FindWindow("Shape").Value == "Rectangle":
+            self.Shapes.append(Rectangle(self.CurrentPen, e.Position.x, e.Position.y))
         e.Skip()
 
     def OnMouseUp(self: wx.Window, e: wx.MouseEvent):
@@ -100,13 +104,10 @@ class OOPDraw(wx.Frame):
 
     def OnMouseMove(self: wx.Window, e: wx.MouseEvent):
         if self.dragging:
-            # Page 15 exercise start
-            currentLine = self.Lines[-1]
-            currentLine.GrowTo(e.GetPosition().x, e.GetPosition().y)
-            # Page 15 exercise end
-            self.lastMousePosition = e.GetPosition()
+            currentLine = self.Shapes[-1]
+            currentLine.GrowTo(e.Position.x, e.Position.y)
+            self.lastMousePosition = e.Position
             self.Refresh()
-    # Page 9 changes end
 
 
 if __name__ == '__main__':
